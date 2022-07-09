ecb2c175237afb5749e391982cdbbf0ac9596ea3
vorvikqq <delodonn@Ukr.net>
2022-07-09 22:16:06 +0300

    cool

---
+++ js/index.js
@@ -0,0 +1,6 @@
+const nav = document.getElementById('nav');
+const but1 = document.getElementById('show');
+const but2 = document.getElementById('hid');
+function setNone() {
+    nav.classList.toggle('flex');
+}
